8c4e9480e12c3e47c1d7a485a50f20c1c12aa075
Kayla-Mujuni <kaylarmj@gmail.com>
2024-08-10 14:06:31 +0300

    END POINTS

--- forms.js
+++ forms.js
@@ -1,12 +1,15 @@
 const express = require ("express")
-
 const mongoose = require ("mongoose")
 require('dotenv/config')
+
+const bodyParser = require('body-parser')
+const routes = require('./routes')
 const app = express()
 
+//app.use(bodyParser.json())
+app.use('/api',routes)
 
-
-mongoose.connect()
+mongoose.connect(process.env.MongoDBlink)
 .then(()=>console.log('CONNECTED TO THE DB'))
 .catch(error=>{console.log('Encountered error', error)})
 
--- routes.js
+++ routes.js
@@ -1,3 +1,27 @@
 const express = require('express')
 
 const router = express.Router()
+
+
+const orders = require('./schema')
+
+// Get method 
+router.get('/',(req,res)=>
+    {res.json({mssg :" Hello"})
+})
+
+module.exports = router
+
+// Post method 
+router.post ('/orders',(req, res)=>{
+const order = new orders( {
+    name:req.body.name ,
+    email: req.body.email,
+     phoneNumber:req.body.phonenumber
+     
+})
+order.save()
+.then(data=>{res.json(data)})
+.catch(err=>{res.json({mss:err})})
+
+} )
